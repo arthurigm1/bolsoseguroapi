133ff64dab1007feff7a06e32544f9e46d493262
ARTHUR IGNACIO MARTINS <95000379+arthurigm1@users.noreply.github.com>
2025-03-08 19:54:31 -0300

    teste

---
+++ src/main/java/bolsoseguroapi/BolsoseguroapiApplication.java
@@ -0,0 +1,13 @@
+package bolsoseguroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsoseguroapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsoseguroapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/bolsoseguroapi/Model/Usuario.java
@@ -0,0 +1,19 @@
+package bolsoseguroapi.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+}
